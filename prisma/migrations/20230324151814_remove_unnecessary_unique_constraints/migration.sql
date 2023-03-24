6c5773ba93bb93b99f1bbd791d12bf21748d6369
Bailey Granam <mpbtgranam@gmail.com>
2023-03-24 09:18:28 -0600

    remove unnecessary unique constraints

---
+++ prisma/migrations/20230324151814_remove_unnecessary_unique_constraints/migration.sql
@@ -0,0 +1,26 @@
+-- DropIndex
+DROP INDEX "cases_id_businessId_key";
+
+-- DropIndex
+DROP INDEX "clients_id_businessId_key";
+
+-- DropIndex
+DROP INDEX "criminal_history_id_clientId_key";
+
+-- DropIndex
+DROP INDEX "entries_id_clientId_key";
+
+-- DropIndex
+DROP INDEX "immigration_history_id_clientId_key";
+
+-- DropIndex
+DROP INDEX "notes_id_caseId_key";
+
+-- DropIndex
+DROP INDEX "referral_sources_id_businessId_key";
+
+-- DropIndex
+DROP INDEX "timesheets_id_caseId_key";
+
+-- DropIndex
+DROP INDEX "users_id_businessId_key";
